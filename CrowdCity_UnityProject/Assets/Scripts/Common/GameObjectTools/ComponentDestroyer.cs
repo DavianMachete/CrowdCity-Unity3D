1b62f97e82cd1bcbf197c4b4e75556ff9593adc9
DavianMachete <71349845+DavianMachete@users.noreply.github.com>
2022-08-09 21:47:38 +0400

    City Updated

---
+++ CrowdCity_UnityProject/Assets/Scripts/Common/GameObjectTools/ComponentDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ComponentDestroyer : MonoBehaviour
+{
+    public Transform myObject;
+    private Collider[] childCollider;
+
+    public void DestroyColliders()
+    {
+        foreach (Transform child in myObject)
+        {
+            childCollider = child.GetComponentsInChildren<Collider>();
+
+            for (int i = 0; i < childCollider.Length; i++)
+            {
+                DestroyImmediate(childCollider[i]);
+            }
+        }
+    }
+}
+
---
+++ CrowdCity_UnityProject/Assets/Scripts/Editor/ComponentDestroyerEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ComponentDestroyer))]
+public class ComponentDestroyerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        ComponentDestroyer myScript = (ComponentDestroyer)target;
+        if (GUILayout.Button("Destroy Colliders")) myScript.DestroyColliders();
+    }
+}
